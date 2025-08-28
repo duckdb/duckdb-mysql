1d8e6ffe2094bef568ac6b7f5b40b9cd3333d440
Alex Kasko <alex@staticlibs.net>
2025-08-28 21:37:07 +0100

    Fix DATE const handling in UPDATE queries
    When `UPDATE` queries are generated for a few types (including `DATE`)
    the incoming constant (in `SET` clause) is converted to `VARCHAR` using
    `Value#ToSQLString()` function. This call adds Postgres'-like cast
    suffix to the literal (like `::DATE`) and this syntax is not supported
    by MySQL/MariaDB.
    
    This PR changes the constant passing in `UPDATE` using the same logic
    that is used for filter pushdowns.
    
    Testing: existing test is updated to cover `UPDATE`s with `DATE`
    literals.
    
    Fixes: #143

--- src/mysql_filter_pushdown.cpp
+++ src/mysql_filter_pushdown.cpp
@@ -39,32 +39,6 @@ string MySQLFilterPushdown::TransformComparison(ExpressionType type) {
 	}
 }
 
-static string TransformBlobToMySQL(const string &val) {
-	char const HEX_DIGITS[] = "0123456789ABCDEF";
-
-	string result = "x'";
-	for (idx_t i = 0; i < val.size(); i++) {
-		uint8_t byte_val = static_cast<uint8_t>(val[i]);
-		result += HEX_DIGITS[(byte_val >> 4) & 0xf];
-		result += HEX_DIGITS[byte_val & 0xf];
-	}
-	result += "'";
-	return result;
-}
-
-string MySQLFilterPushdown::TransformConstant(const Value &val) {
-	if (val.type().IsNumeric()) {
-		return val.ToSQLString();
-	}
-	if (val.type().id() == LogicalTypeId::BLOB) {
-		return TransformBlobToMySQL(StringValue::Get(val));
-	}
-	if (val.type().id() == LogicalTypeId::TIMESTAMP_TZ) {
-		return val.DefaultCastAs(LogicalType::TIMESTAMP).DefaultCastAs(LogicalType::VARCHAR).ToSQLString();
-	}
-	return val.DefaultCastAs(LogicalType::VARCHAR).ToSQLString();
-}
-
 string MySQLFilterPushdown::TransformFilter(string &column_name, TableFilter &filter) {
 	switch (filter.filter_type) {
 	case TableFilterType::IS_NULL:
@@ -81,7 +55,7 @@ string MySQLFilterPushdown::TransformFilter(string &column_name, TableFilter &fi
 	}
 	case TableFilterType::CONSTANT_COMPARISON: {
 		auto &constant_filter = filter.Cast<ConstantFilter>();
-		auto constant_string = TransformConstant(constant_filter.constant);
+		auto constant_string = MySQLUtils::TransformConstant(constant_filter.constant);
 		auto operator_string = TransformComparison(constant_filter.comparison_type);
 		return StringUtil::Format("%s %s %s", column_name, operator_string, constant_string);
 	}
@@ -99,7 +73,7 @@ string MySQLFilterPushdown::TransformFilter(string &column_name, TableFilter &fi
 			if (!in_list.empty()) {
 				in_list += ", ";
 			}
-			in_list += TransformConstant(val);
+			in_list += MySQLUtils::TransformConstant(val);
 		}
 		return column_name + " IN (" + in_list + ")";
 	}
